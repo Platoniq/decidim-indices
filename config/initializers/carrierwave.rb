e6b9df7ddbe9478c245841254d3b0a68e00ae6e5
Ivan Vergés <ivan@platoniq.net>
2020-03-09 20:11:29 +0100

    sidekiq queues

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,22 +10,26 @@ CarrierWave.configure do |config|
 end
 
 # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
-#
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-#   config.fog_provider = 'fog/aws'                                             # required
-#   config.fog_credentials = {
-#     provider:              'AWS',                                             # required
-#     aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
-#     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
-#     region:                'eu-west-1',                                       # optional, defaults to 'us-east-1'
-#     host:                  's3.example.com',                                  # optional, defaults to nil
-#     endpoint:              'https://s3.example.com:8080'                      # optional, defaults to nil
-#   }
-#   config.fog_directory  = 'name_of_directory'                                 # required
-#   config.fog_public     = false                                               # optional, defaults to true
-#   config.fog_attributes = {
-#     'Cache-Control' => "max-age=#{365.day.to_i}",
-#     'X-Content-Type-Options' => "nosniff"
-#   }
-# end
+
+if Rails.application.secrets.aws_access_key_id.present?
+  require "carrierwave/storage/fog"
+
+	CarrierWave.configure do |config|
+	  config.storage = :fog
+	  config.fog_provider = 'fog/aws'                                             # required
+	  config.fog_credentials = {
+	    provider:              'AWS',                                             # required
+	    aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
+	    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
+	    region:                'eu-west-1',                                       # optional, defaults to 'us-east-1'
+	    host:                  's3.eu-west-1.amazonaws.com',                                  # optional, defaults to nil
+	    # endpoint:              'https://s3.example.com:8080'                      # optional, defaults to nil
+	  }
+	  config.fog_directory  = ENV.fetch("AWS_BUCKET_NAME", 'decidim-indices')     # required
+	  # config.fog_public     = false                                               # optional, defaults to true
+	  config.fog_attributes = {
+	    'Cache-Control' => "max-age=#{365.day.to_i}",
+	    'X-Content-Type-Options' => "nosniff"
+	  }
+	end
+end
