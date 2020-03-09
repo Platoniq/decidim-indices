30de046a308f784e4fd982d0ee970d3bce7d70e8
Ivan Vergés <ivan@platoniq.net>
2020-03-09 20:59:21 +0100

    sidekiq admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
+# frozen_string_literal: true
+
+require "sidekiq/web"
+require "sidekiq/cron/web"
+
 Rails.application.routes.draw do
+
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => "/sidekiq"
+  end
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
