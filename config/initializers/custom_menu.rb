8f482478731cd196328d02083c8bfbd65e31b47c
Ivan Vergés <ivan@platoniq.net>
2020-04-23 11:17:57 +0200

    add blog menu entry

---
+++ config/initializers/custom_menu.rb
@@ -0,0 +1,11 @@
+Decidim.menu :menu do |menu|
+  if Rails.application.secrets.menu[current_organization.host.to_sym].respond_to? :each
+    Rails.application.secrets.menu[current_organization.host.to_sym].each do |tenant, item|
+      options = {}
+      options[:position] = item[:position].to_i if item[:position]
+      options[:active] = item[:active].to_sym if item[:active]
+      options[:icon_name] = item[:icon_name].to_s if item[:icon_name]
+      menu.item I18n.t(item[:key]), item[:url], options
+    end
+  end
+end
