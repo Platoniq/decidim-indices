ff75ff60886f0efef541c68133bce959c1542c7e
Ivan Vergés <ivan@platoniq.net>
2021-04-05 11:38:34 +0200

    update gems. move sentry to an initializer

--- config/application.rb
+++ config/application.rb
@@ -19,14 +19,3 @@ module Indices
     # the framework and any gems in your application.
   end
 end
-
-unless ENV["DISABLE_SENTRY"]
-  Sentry.init do |config|
-    config.dsn = ENV["SENTRY_DSN"]
-    config.breadcrumbs_logger = [:active_support_logger]
-
-    # To activate performance monitoring, set one of these options.
-    # We recommend adjusting the value in production:
-    config.traces_sample_rate = 0.5
-  end
-end
