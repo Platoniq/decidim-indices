46106bb6248f5e10f78331abdb472172a9dbb9c6
Ivan Vergés <ivan@platoniq.net>
2022-03-03 19:02:02 +0100

    fix mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.default_url_options = { port: 3000 }
+  config.action_mailer.asset_host = "http://localhost:3000"
 
   config.action_mailer.perform_caching = false
 
