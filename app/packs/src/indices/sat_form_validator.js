1d5637f4a92988b027c19ed153418d0caf8f8325
Agusti B.R <agustibr@users.noreply.github.com>
2022-05-20 15:06:36 +0200

    Chore/sat survey (#72)
    * chore(questionnaire): wider space for survey q&a
    
    * feat(answers): add short and log answer fields
    
    * chore(sat_validator): improve js checkbox colection

--- app/packs/src/indices/sat_form_validator.js
+++ app/packs/src/indices/sat_form_validator.js
@@ -18,11 +18,11 @@ $(() => {
 
   let disableBtnContinue = () => {
     $buttonNext().addClass("disabled");
-    console.log('🍅 disabled Continue');
+    // console.log('🍅 disabled Continue');
   };
 
   let enableBtnContinue = () => {
-    console.log('🍏 Continue');
+    // console.log('🍏 Continue');
     $buttonNext().removeClass("disabled");
   };
 
@@ -51,21 +51,23 @@ $(() => {
   }
 
   let showValidationAlert = () => {
-
     $visibleErrors().each(function(i){
       let $element = $(this)
       let $fieldset = $element.closest(".js--sat-mandatory-fieldset");
       let $alert = $fieldset.find(".js--sat-fieldset-error");
       $alert.removeClass("hide");
+      $element.closest(".check-box-collection").addClass("is-invalid-label");
+
     });
   }
 
   let hideValidationAlert = () => {
-    console.log("--------> hidealert")
+    // console.log("--------> hidealert")
     $survey.find(".js--sat-mandatory-fieldset").each(function(i){
       let $fieldset = $(this)
       let $alert = $fieldset.find(".js--sat-fieldset-error");
       $alert.addClass("hide");
+      $fieldset.find(".check-box-collection").removeClass("is-invalid-label");
     });
   }
 
@@ -84,34 +86,34 @@ $(() => {
 
 
   $survey.find(".js--one-step-beyond").on("click", function(ev) {
-    console.log('⏩ [click next] validate form!');
+    // console.log('⏩ [click next] validate form!');
     validateStep();
-    console.log("❓ hasErrorsCurrentStep", $hasErrorsCurrentStep())
+    // console.log("❓ hasErrorsCurrentStep", $hasErrorsCurrentStep())
     if($hasErrorsCurrentStep()) {
-      console.log("👎 the step has errors")
+      // console.log("👎 the step has errors")
       ev.stopImmediatePropagation();
       ev.preventDefault();
     } else {
-      console.log("👍 the step is valid")
+      // console.log("👍 the step is valid")
     }
   });
 
   $sumbitButton.on("click", function(ev) {
-    console.log('⏩ [click SEND] validate form!');
+    // console.log('⏩ [click SEND] validate form!');
     validateStep();
-    console.log("❓ hasErrorsCurrentStep", $hasErrorsCurrentStep())
+    // console.log("❓ hasErrorsCurrentStep", $hasErrorsCurrentStep())
     if($hasErrorsCurrentStep()) {
-      console.log("👎 the FORM has errors")
+      // console.log("👎 the FORM has errors")
       ev.stopImmediatePropagation();
       ev.preventDefault();
     } else {
-      console.log("👍 the FORM is valid")
+      // console.log("👍 the FORM is valid")
     }
   })
 
   // when an input is invalid
   $survey.on("invalid.zf.abide", function(ev,frm) {
-    console.log(" ❌ Input id "+ev.target.id+" is invalid :(");
+    // console.log(" ❌ Input id "+ev.target.id+" is invalid :(");
     if($hasErrorsCurrentStep()) {
       disableBtnContinue();
       disableSubmitBtn();
@@ -125,7 +127,7 @@ $(() => {
 
   // when an input is valid
   $survey.on("valid.zf.abide", function(ev,frm) {
-    console.log("✅ Input id "+ev.target.id+" is valid");
+    // console.log("✅ Input id "+ev.target.id+" is valid");
     if($hasErrorsCurrentStep()) {
       disableBtnContinue();
       disableSubmitBtn();
@@ -139,13 +141,13 @@ $(() => {
 
   // form validation failed
   $survey.on("forminvalid.zf.abide", function(ev,frm) {
-    console.log("🚫 Form id "+ev.target.id+" is invalid");
+    // console.log("🚫 Form id "+ev.target.id+" is invalid");
     disableSubmitBtn();
   })
 
   // form validation passed, form will submit if submit event not returned false
   $survey.on("formvalid.zf.abide", function(ev,frm) {
-    console.log("📬 Form id "+frm.attr('id')+" is valid");
+    // console.log("📬 Form id "+frm.attr('id')+" is valid");
     enableSubmitBtn();
   });
 
