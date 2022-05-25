410e9d8ee5e5c4466ef3129a9068502bdfd420fb
Agusti B.R <hola@agusti.cat>
2022-05-25 13:06:45 +0200

    chore(sentry): update breadcrumbs_logger

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,7 @@
 if ENV["SENTRY_DSN"]
   Sentry.init do |config|
     config.dsn = ENV["SENTRY_DSN"]
-    config.breadcrumbs_logger = [:active_support_logger]
+    config.breadcrumbs_logger = [:active_support_logger, :sentry_logger, :http_logger]
     config.traces_sample_rate = 0.5
   end
 end
