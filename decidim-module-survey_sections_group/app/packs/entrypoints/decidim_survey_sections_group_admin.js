080c21b8ee963fd36b526bd9618649b69e74a963
Alejandro Puerta <alejandro.puerta@nazaries.com>
2022-11-24 16:46:56 +0100

    fix: entrypoint survey_sections_group_admin

--- decidim-module-survey_sections_group/app/packs/entrypoints/decidim_survey_sections_group_admin.js
+++ decidim-module-survey_sections_group/app/packs/entrypoints/decidim_survey_sections_group_admin.js
@@ -1,4 +1,4 @@
-import "src/decidim/survey_sections/admin/collapsible_questions"
+import "src/decidim/survey_sections_group/admin/collapsible_questions"
 
-import createEditableForm from "../src/decidim/survey_sections/admin/survey_sections"
+import createEditableForm from "../src/decidim/survey_sections_group/admin/survey_sections_group"
 window.Decidim.createEditableForm = createEditableForm
