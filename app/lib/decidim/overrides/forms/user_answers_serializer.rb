e4bad740d2ccf145146cc1e2a27a737c1d7baf9c
Alejandro Puerta <71900287+apuertadelaguila@users.noreply.github.com>
2023-03-29 12:50:58 +0200

    Export all survey answers as xlsx job (#133)
    * feat: job in order to export all surveys answer of a specific survey_sections_group
    
    * fix: user_id added with md5 encrypt, serializer for export_questionnaire_answers_csv_job.rb fixed
    
    * feat: export_survey_answers_as_xlsx_job added
    
    * fix: rubocop fixes

--- app/lib/decidim/overrides/forms/user_answers_serializer.rb
+++ app/lib/decidim/overrides/forms/user_answers_serializer.rb
@@ -4,15 +4,14 @@ module Decidim
   module Overrides
     module Forms
       module UserAnswersSerializer
-        # Public: Exports a hash with the serialized data for the user answers.
-        def serialize
-          answers_hash = questions_hash
-
-          @answers.each do |answer|
-            answers_hash[translated_question_key(answer.question.position, answer.question.body)] = normalize_body(answer)
-          end
-
-          answers_hash
+        def hash_for(answer)
+          {
+            answer_translated_attribute_name(:user_identifier) => answer&.decidim_user_id.present? ? Digest::MD5.hexdigest(answer&.decidim_user_id.to_s) : "Unknown",
+            answer_translated_attribute_name(:id) => answer&.session_token,
+            answer_translated_attribute_name(:created_at) => answer&.created_at&.to_s(:db),
+            answer_translated_attribute_name(:ip_hash) => answer&.ip_hash,
+            answer_translated_attribute_name(:user_status) => answer_translated_attribute_name(answer&.decidim_user_id.present? ? "registered" : "unregistered")
+          }
         end
       end
     end
