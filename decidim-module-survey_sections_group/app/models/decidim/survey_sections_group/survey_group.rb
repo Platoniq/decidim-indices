bd4e705b3ae9c810b87b253dfcc13054e66d5443
Alejandro Puerta <alejandro.puerta@nazaries.com>
2023-03-30 09:49:03 +0200

    fix: previous_survey_for and next_survey_for methods take now the survey not answered by the current user with the closed weight in the same survey_section

--- decidim-module-survey_sections_group/app/models/decidim/survey_sections_group/survey_group.rb
+++ decidim-module-survey_sections_group/app/models/decidim/survey_sections_group/survey_group.rb
@@ -13,14 +13,17 @@ module Decidim
         self.class.where(decidim_survey_section_id: survey_section.id)
             .joins(:survey)
             .where("decidim_components.weight < ?", survey.weight)
-          &.find { |s| !s.survey.answered_by?(user.id) }
+            .sort_by { |s| s.survey.weight }
+            .reverse
+            &.find { |s| !s.survey.answered_by?(user.id) }
       end
 
       def next_survey_for(user)
         self.class.where(decidim_survey_section_id: survey_section.id)
             .joins(:survey)
             .where("decidim_components.weight > ?", survey.weight)
-          &.find { |s| !s.survey.answered_by?(user.id) }
+            .sort_by { |s| s.survey.weight }
+            &.find { |s| !s.survey.answered_by?(user.id) }
       end
     end
   end
